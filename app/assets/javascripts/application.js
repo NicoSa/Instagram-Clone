1b7158ce0382370bf8d5d6dd63da98dd77e2f9f1
Nico Saueressig <nico@nicosaueressig.de>
2014-06-02 12:49:05 +0100

    FINALLY like/unlike working and removing user as well

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,15 +18,17 @@
 //= require mustache
 
 $(document).ready(function() {
-    $('body').on('click', '.like', function() {
+    $('body').on('click', '.like', function(event) {
+        event.preventDefault();
         $.post($(this).attr('href'), $(this).serialize(), function(response) {
 
             var targetId = response.post
             var currentPost = $('.col-md-4[data-id=' + targetId + ']')
 
             var template = $('#likes-template').html();
+            var output = Mustache.render(template, response)
 
-            currentPost.find('.likes').append(response.user + ", ");
+            currentPost.find('.likes').append(output);
             currentPost.find('.like').replaceWith(response.unlike);
         }, 'json');
         return false;
@@ -38,8 +40,8 @@ $(document).ready(function() {
             type: 'DELETE',
             success: function(response) {
                 var targetId = response.post;
-                var currentPost = $(".post[data-id=" + targetId + "]");
-
+                var currentPost = $(".post[data-id='" + targetId + "']");
+                currentPost.find(".each-likes[data-id='" + response.user.email + "']").remove();
                 currentPost.find('.unlike').replaceWith(response.like);
             }
         });
