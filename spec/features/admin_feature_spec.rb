dd3c577fab0b8e5dd905afe4556cc67f93694f48
Nico Saueressig <nico@nicosaueressig.de>
2014-06-03 15:58:19 +0100

    admin login in place

--- spec/features/admin_feature_spec.rb
+++ spec/features/admin_feature_spec.rb
@@ -1,8 +1,8 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe 'admins' do
+describe 'admins' do
 	
-# 	it 'cannot sign up' do
-# 		expect{ visit '/admins/sign_up'}.to raise_error
-# 	end
-# end
+	it 'cannot sign up' do
+		expect{ visit '/admins/sign_up'}.to raise_error
+	end
+end
