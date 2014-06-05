6e950ed5a25a4ac4e0a2660cb55bcf606ea2f909
Nico Saueressig <nico@nicosaueressig.de>
2014-06-05 17:02:54 +0100

    map can automatically geolocated or you type in your location manually markers disappear when new one is put

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,30 +1,33 @@
 $(document).ready(function() {
     if ($('#new_post').length) {
+        var map = new GMaps({
+            div: '#map-posts',
+            lat: 16.771974,
+            lng: -3.015361,
+            click: function(event) {
+                console.log(event);
+                map.removeMarkers();
+                map.addMarker({
+                    lat: event.latLng.lat(),
+                    lng: event.latLng.lng(),
+
+                });
+                $('.longitude').val(event.latLng.lng());
+                $('.latitude').val(event.latLng.lat());
+            }
+
+        });
+
         GMaps.geolocate({
             success: function(position) {
-                //map.setCenter(position.coords.latitude, position.coords.longitude);
                 var latitude_data = position.coords.latitude;
                 var longitude_data = position.coords.longitude;
-                // console.log(latitude_data);
-                // console.log(longitude_data);
+                map.setCenter(latitude_data, longitude_data)
+
 
                 $('.longitude').val(longitude_data);
                 $('.latitude').val(latitude_data);
-                var map = new GMaps({
-                    div: '#map-posts',
-                    lat: latitude_data,
-                    lng: longitude_data,
-                    click: function(event) {
-                        console.log(event);
-                        map.removeMarkers();
-                        map.addMarker({
-                            lat: event.latLng.lat(),
-                            lng: event.latLng.lng()
-                        });
-                        $('.longitude').val(event.latLng.lng());
-                        $('.latitude').val(event.latLng.lat());
-                    }
-                });
+
                 map.addMarker({
                     lat: latitude_data,
                     lng: longitude_data
@@ -36,6 +39,25 @@ $(document).ready(function() {
             not_supported: function() {
                 alert("Your browser does not support geolocation");
             }
+        });
+        $('#submit-location-search').on('click', function() {
+            map.removeMarkers();
+            GMaps.geocode({
+                address: $('#address').val(),
+                callback: function(results, status) {
+                    if (status == 'OK') {
+                        var latlng = results[0].geometry.location;
+                        map.setCenter(latlng.lat(), latlng.lng());
+
+                        map.addMarker({
+                            lat: latlng.lat(),
+                            lng: latlng.lng()
+                        });
+                    }
+                    $('.longitude').val(latlng.lat());
+                    $('.latitude').val(latlng.lng());
+                }
+            });
         });
     };
 
